f1f57bc875d2648117353ca7462aec99326ec8cc
dilzad345 <59992266+dilzad345@users.noreply.github.com>
2020-01-24 11:22:28 +0530

    dog.java

---
+++ dog.java
@@ -0,0 +1,15 @@
+class dog {
+private string name;
+private  int age
+private string color;
+
+dog() {
+this("browny, 12, "brown");
+}
+
+dog(string name, int age, string color) {
+this.name = name;
+this.age = age;
+this.color = color;
+
+}
